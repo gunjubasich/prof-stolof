cd68b76b93118c60a0f6cf471febed93d91e6a18
gunjubasich <gunjubasich@gmail.com>
2017-07-06 12:44:38 +0500

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,26 +1,14 @@
 $(function() {
 
-	$(document).ready(function() {
-  
-  //Open
-  $('.button-modal').click(function() {
-    $('.modal').addClass('is-open');
+	$('#owl-hero').owlCarousel({
+		items:1,
+	    loop:true,
+	    autoplay:true,
+	    autoplayTimeout:5000,
+	    nav:true,
+      autoHeight:true,
+	    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    }); 
+
+
   });
-  
-  //Close
-  $('.modal-close').click(function() {
-    $('.modal').removeClass('is-open');
-  });
-  
-  // Color
-  $(".colors-list li").click(function(){
-    $(this).addClass("color-selected").siblings(".colors-list li").removeClass("color-selected");
-  });
-  
-  // Size
-  $(".sizes-list li").click(function(){
-    $(this).addClass("size-selected").siblings(".sizes-list li").removeClass("size-selected");
-  });
-});
-	
-});
